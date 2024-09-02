2de4445b49f80c22e28e132461cd2063931a002e
Ekaterina-Alekseeva44 <Caterinazh@mail.ru>
2024-09-02 23:02:46 +0300

    Add files via upload

---
+++ js/1.js
@@ -0,0 +1,65 @@
+function getOpenBtn() {
+    let openBtn = document.createElement("button")
+    openBtn.classList.add("open-btn")
+    openBtn.textContent = "Открыть меню"
+
+    openBtn.onclick = function () {
+        if (menu.classList.contains("open") == true) {
+            openBtn.textContent = "Открыть меню"
+            menu.classList.remove("open")
+        } else {
+            openBtn.textContent = "Закрыть меню"
+            menu.classList.add("open")
+        }
+    }
+
+    return openBtn
+}
+
+let menuArray = ["Главная страница", "О нас", "Услуги", "Контакты", "Вход"]
+let menuHrefArray = ["https://dzen.ru", "https://dzen.ru", "https://dzen.ru", "https://dzen.ru", "https://dzen.ru"]
+
+function getMenu(itemsArray, hrefArray) {
+    let menuList = document.createElement("ul")
+    menuList.classList.add("menu")
+
+
+    for (let i = 0; i < itemsArray.length; i++) {
+        let item = document.createElement("li")
+        item.classList.add("menu__item")
+
+        let link = document.createElement("a")
+        link.classList.add("menu__link")
+        link.textContent = itemsArray[i]
+        link.href = hrefArray[i]
+
+        item.append(link)
+        menuList.append(item)
+    }
+
+    return menuList
+
+}
+
+let menu = getMenu(menuArray, menuHrefArray)
+document.body.append(getOpenBtn(), menu)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//То,что было изначально до создания функции
+//let openBtn = document.createElement("button")
+//openBtn.classList.add("open-btn")
+//openBtn.textContent = "Открыть меню"
+
+//document.body.append(openBtn)
